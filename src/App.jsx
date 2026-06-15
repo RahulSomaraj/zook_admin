f8bba706e7bde1f15617d724007a9d42ec87ae3d
Anaswar4 <anaswarpk13@gmail.com>
2026-06-15 11:59:22 +0530

    Fraud Review page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import ProductCatalog from "./pages/ProductCatalog";
+import FraudReview from "./pages/FraudReview";
 
 export default function App() {
   return (
     <MainLayout>
-      <ProductCatalog />
+      <Routes>
+        <Route path="/" element={<Navigate to="/product-catalog" replace />} />
+        <Route path="/product-catalog" element={<ProductCatalog />} />
+        <Route path="/fraud-review" element={<FraudReview />} />
+      </Routes>
     </MainLayout>
   );
 }
